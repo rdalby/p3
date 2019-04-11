bfebeb62ce1444eaf02c03b42b9a2380aa536ac6
Rachel Dalby <rdalby@hsph.harvard.edu>
2019-04-10 22:36:38 -0400

    finishing up p3 code

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,16 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//This actually calls the method to process the form
+Route::post('/media/media-process', 'MediaController@mediaProcess');
 
-Route::get('/media', 'MediaController@index');
+//This calls the media controller and displays the results page
+Route::get('/media', 'MediaController@mediaRequest');
+
+
+
+//This view route will return the welcome view
+Route::view('/', 'welcome');
+
